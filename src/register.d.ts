0ab66971b51280c843a345523a12c673ce2c2191
Tomas Dostal <Dostal.Tomas@gmail.com>
2018-06-20 16:31:15 +0200

    Fix typings

---
+++ src/register.d.ts
@@ -0,0 +1,38 @@
+import {IModule} from 'angular';
+
+export = register;
+
+declare function register(moduleName: string, dependencies?: string[]): Register;
+
+declare class Register {
+
+    private constructor(moduleName: string, dependencies?: string[]);
+
+    public directive(directiveName: string, directiveClass): this;
+
+    public controller(controllerName: string, controllerClass): this;
+
+    public service(serviceName: string, serviceClass): this;
+
+    public provider(providerName: string, providerClass): this;
+
+    public factory(factoryName: string, factoryFunction: (...args: any[]) => any): this;
+
+    public filter(filterName: string, filterFunction: (...args: any[]) => any): this;
+
+    public component(componentName: string, componentClass): this;
+
+    public constant(name: string, obj: any): this;
+
+    public value(name: string, obj: any): this;
+
+    public decorator(name: string, decoratorFunction: (...args: any[]) => any): this;
+
+    public config(configFunction: (...args: any[]) => any): this;
+
+    public run(initializationFunction: (...args: any[]) => any): this;
+
+    public module(): IModule;
+
+    public name(): string;
+}
